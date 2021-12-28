156f81bc76f7bdc4fa4ec18d0f43cc3d5e9f053d
Ivan Dubrovin <Ivan.Dubrovin@skoltech.ru>
2021-12-28 15:54:52 +0300

    Add script to generate the ROI map

---
+++ Scripts/utils_constants.R
@@ -0,0 +1,71 @@
+# Load the data and manually define the constants.
+
+library(sf)
+library(tidyverse)
+
+vectors <- file.path("..", "Data", "vectors")
+
+russia <- geodata::gadm('Russia', path = vectors, level = 2)
+russia <- sf::st_as_sf(russia)
+
+kamchatka <- filter(russia, NAME_1 == "Kamchatka")
+kamchatka <- st_transform(kamchatka, 32657)
+
+elizovskiy_rayon <- filter(kamchatka, NAME_2 == "Elizovskiy rayon")
+
+# Constants for the ROI map
+avacha_bay <- st_polygon(
+  list(
+    rbind(
+      c(445000, 5900000),
+      c(545000, 5900000),
+      c(545000, 5800000),
+      c(445000, 5800000),
+      c(445000, 5900000)
+    )
+  )
+) |>
+  st_sfc(crs = 32657)
+
+khalaktyrsky_beach <- st_point(c(158.859018, 52.996211)) |>
+  st_sfc(crs = 4326) |>
+  st_as_sf() |>
+  mutate(name = "Khalaktyrsky beach") |>
+  st_transform(crs = 32640)
+
+avacha_bay_point <- st_point(c(159.136409, 52.850091)) |>
+  st_sfc(crs = 4326) |>
+  st_as_sf() |>
+  mutate(name = "Avacha Bay") |>
+  st_transform(crs = 32640)
+
+seas_coords <- rbind(
+  c(170.333862, 57.250402),
+  c(167.272312, 51.605518)
+)
+
+seas_names <- c(
+  "Bering\nSea",
+  "Pacific\nOcean"
+)
+
+seas <- st_multipoint(matrix(seas_coords, ncol = 2)) |>
+  st_sfc(crs = 4326) |>
+  st_cast(to = "POINT") |>
+  st_as_sf() |>
+  mutate(name = seas_names)
+
+cities_coords <- rbind(
+  c(158.643498, 53.024261)
+)
+
+cities_names <- c(
+  "Petropavlovsk-\nKamchatsky"
+)
+
+cities <- st_multipoint(matrix(cities_coords, ncol = 2)) |>
+  st_sfc(crs = 4326) |>
+  st_cast(to = "POINT") |>
+  st_as_sf() |>
+  mutate(name = cities_names)
+
